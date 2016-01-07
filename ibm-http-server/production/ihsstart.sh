6511d65c9ab3a7c13fb15b67a8d41966a049447a
Kavitha Suresh Kumar <kavisuresh@in.ibm.com>
2016-01-07 14:14:02 +0000

    Dockerfiles updated

--- ibm-http-server/production/ihsstart.sh
+++ ibm-http-server/production/ihsstart.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
+#####################################################################################
+#                                                                                   #
+#  Script to start the server                                                       #
+#                                                                                   #
+#                                                                                   #
+#  Usage : ihsstart.sh                                                              #
+#                                                                                   #
+#  Author : Kavitha                                                                 #
+#                                                                                   #
+#####################################################################################
 
+# Starting IBM HTTPServer
 /opt/IBM/HTTPServer/bin/apachectl start
 
-/opt/IBM/HTTPServer/bin/adminctl start
+echo "IBM HTTP Server started successfully"
 
 while [ `ps -eaf | grep httpd | wc -l` > 4 ]
 do
