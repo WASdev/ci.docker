62b6df1e484968cb9f47da75e9ffcfa227c4e275
David Currie <david_currie@uk.ibm.com>
2016-04-12 15:46:43 +0100

    Add restart test

--- websphere-liberty/test/verify.sh
+++ websphere-liberty/test/verify.sh
@@ -12,7 +12,25 @@ image=$1
 tag=`echo $image | cut -d ":" -f2`
 cname="${tag}test"
 
-testContainerRuns()
+waitForServerStart()
+{
+   cid=$1
+   count=${2:-1}
+   end=$((SECONDS+30))
+   while (( $SECONDS < $end && $(docker inspect -f {{.State.Running}} $cid) == "true" ))
+   do
+      result=$(docker logs $cid |& grep "CWWKF0011I" | wc -l)
+      if [ $result = $count ]
+      then
+         return 0
+      fi
+   done
+
+   echo "Liberty failed to start the expected number of times"
+   return 1 
+}
+
+testLibertyStarts()
 {
    cid=$(docker run -d $image)
    if [ $? != 0 ]
@@ -21,76 +39,103 @@ testContainerRuns()
       exit 1
    fi
 
-   while [ $(docker inspect -f {{.State.Running}} $cid) = "true" ]
-   do
-      docker logs $cid | grep "ERROR"
-      if [ $? = 0 ]
-      then
-         echo "Errors found in logs for container; exiting"
-         docker rm -f $cid
-         exit 1
-      fi
-      docker logs $cid | grep -i "CWWKF0011I"
-      if [ $? = 0 ]
-      then
-         echo "Container started successfully"
+   waitForServerStart $cid
+   if [ $? != 0 ]
+   then
+      echo "Liberty failed to start; exiting"
+      exit 1
+   fi
 
-         docker stop $cid
-         if [ $? != 0 ]
-         then
-            echo "Container failed to stop cleanly; exiting"
-            docker rm -f $cid
-            exit 1
-         fi
+   docker logs $cid |& grep "ERROR"
+   if [ $? = 0 ]
+   then
+      echo "Errors found in logs for container; exiting"
+      docker rm -f $cid >/dev/null
+      exit 1
+   fi
 
-         docker logs $cid | grep -i "CWWKE0036I"
-         if [ $? != 0 ]
-         then
-            echo "Liberty failed to stop cleanly; exiting"
-            docker logs $cid
-            docker rm -f $cid
-            exit 1
-         fi
-
-         docker rm -f $cid
-         return
-      fi
-   done
-
-   echo "Container exited prematurely; exiting"
-   docker logs $cid
-   docker rm -f $cid
-   exit 1
+   docker rm -f $cid >/dev/null
 }
+
+testLibertyStops()
+{
+   cid=$(docker run -d $image)
+   waitForServerStart $cid
+
+   result=$(docker stop $cid)
+   if [ $? != 0 ]
+   then
+      echo "Container failed to stop cleanly: $result; exiting"
+      docker rm -f $cid >/dev/null
+      exit 1
+   fi
+
+   docker logs $cid | grep -iq "CWWKE0036I"
+   if [ $? != 0 ]
+   then
+      echo "Liberty failed to stop cleanly; exiting"
+      docker logs $cid
+      docker rm -f $cid >/dev/null
+      exit 1
+   fi
+
+   docker rm -f $cid >/dev/null
+}
+
+testLibertyStopsAndRestarts()
+{
+   cid=$(docker run -d $image)
+   waitForServerStart $cid
+   docker stop $cid >/dev/null
+   
+   docker start $cid >/dev/null
+   if [ $? != 0 ]
+   then
+      echo "Failed to run container; exiting"
+      exit 1
+   fi
+   
+   waitForServerStart $cid 2
+   if [ $? != 0 ]
+   then
+      echo "Server failed to restart; exiting"
+      exit 1
+   fi
+
+   docker logs $cid |& grep "ERROR"
+   if [ $? = 0 ]
+   then
+      echo "Errors found in logs for container; exiting"
+      docker rm -f $cid >/dev/null
+      exit 1
+   fi
+
+   docker rm -f $cid >/dev/null
+}
+
 
 testFeatureList()
 {
    if [ $tag = "kernel" ]
    then
-       return
+      return
    fi
 
    features=$(docker run --rm $image /opt/ibm/wlp/bin/productInfo featureInfo | cut -d " " -f1)
    comparison=$(diff -u <(echo "$features") "$tag.txt")
 
-   if [ $? = 0 ]
+   if [ $? != 0 ]
    then
-   	echo "Correct features installed"
-   else
-        echo "Incorrect features installed, exiting"
-        echo "$comparison"
-        exit 1
+      echo "Incorrect features installed, exiting"
+      echo "$comparison"
+      exit 1
    fi
 }
 
 tests=$(declare -F | cut -d" " -f3 | grep "test")
 for name in $tests
 do
-   echo "******************************************************************************"
-   echo "                     Executing $name"
-   echo "******************************************************************************"
+   echo "*** $name - Executing"
    eval $name
-   echo "******************************************************************************"
-   echo "                       $name Completed Successfully                           "
-   echo "******************************************************************************"
+   echo "*** $name - Completed successfully"
 done
