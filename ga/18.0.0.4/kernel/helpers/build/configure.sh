d25dd246c6e635992d1ac01f4cbea8545e0b2d14
leochr <leojc@ca.ibm.com>
2019-01-11 16:18:10 -0500

    Add config snippets for monitoring, health, ssl and http endpoint options

--- ga/18.0.0.4/kernel/helpers/build/configure.sh
+++ ga/18.0.0.4/kernel/helpers/build/configure.sh
@@ -6,10 +6,29 @@ SNIPPETS_TARGET=/config/configDropins/overrides
 
 #Check for each Liberty value-add functionality
 
-#if [ "$MONITORING" == "true" ]
-#then
-# cp $SNIPPETS_SOURCE/monitoring.xml $SNIPPETS_TARGET/monitoring.xml
-#fi
+# MicroProfile Health
+if [ "$MP_HEALTH_CHECK" == "true" ]; then
+  cp $SNIPPETS_SOURCE/mp_health_check.xml $SNIPPETS_TARGET/mp_health_check.xml
+fi
+
+# MicroProfile Monitoring
+if [ "$MP_MONITORING" == "true" ]; then
+  cp $SNIPPETS_SOURCE/mp_monitoring.xml $SNIPPETS_TARGET/mp_monitoring.xml
+fi
+
+# SSL
+if [ "$SSL" == "true" ]; then
+  cp $SNIPPETS_SOURCE/ssl.xml $SNIPPETS_TARGET/ssl.xml
+fi
+
+# HTTP Endpoint
+if [ "$HTTP_ENDPOINT" == "true" ]; then
+  if [ "$SSL" == "true" ]; then
+    cp $SNIPPETS_SOURCE/https-http-endpoint.xml $SNIPPETS_TARGET/https-http-endpoint.xml
+  else
+    cp $SNIPPETS_SOURCE/http-endpoint.xml $SNIPPETS_TARGET/http-endpoint.xml
+  fi
+fi
 
 # Install needed features
 installUtility install --acceptLicense defaultServer
