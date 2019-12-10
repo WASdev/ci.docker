ac373dc4357163f6b05c9d962596c974c11789b3
Justeen-Randev <Justeen.Randev@ibm.com>
2019-12-10 15:32:40 -0500

    Building latest images

--- test/buildAll.sh
+++ test/buildAll.sh
@@ -12,6 +12,7 @@
 
 currentRelease=$1
 readonly REPO="websphere-liberty"
+readonly IMAGE_ROOT="ga/latest"
 
 # Default to podman where available, docker otherwise.
 # Override by setting the DOCKER environment variable.
@@ -42,22 +43,20 @@ if [[ $1 =~ ^\.\.\/ga\/19\.0\.0\.[69]$ ]]; then
   done < $currentRelease/"images.txt"
 else 
   echo "${currentRelease}"
+  file_exts_ubi=(ubi.adoptopenjdk8 ubi.adoptopenjdk11 ubi.ibmjava8 ubuntu.ibmjava8)
+  tag_exts_ubi=(java8-openj9-ubi java11-openj9-ubi java8-ibmjava-ubi java8-ibmjava)
+
+  for i in "${!tag_exts_ubi[@]}"; do
+      docker_dir="${IMAGE_ROOT}/kernel"
+      full_path="${docker_dir}/Dockerfile.${file_exts_ubi[$i]}"
+      if [[ -f "${full_path}" ]]; then
+          build_tag="${REPO}:full-${tag_exts_ubi[$i]}"
+
+          echo "****** Building image ${build_tag}..."
+          $DOCKER build --no-cache=true "${build_tag}" -f "${full_path}"
+      else
+          echo "Could not find Dockerfile at path ${full_path}"
+          exit 1
+      fi
+  done
 fi
-# else
-#     file_exts_ubi=(ubi.adoptopenjdk8 ubi.adoptopenjdk11 ubi.ibmjava8 ubuntu.ibmjava8)
-#     tag_exts_ubi=(java8-openj9-ubi java11-openj9-ubi java8-ibmjava-ubi java8-ibmjava)
-
-#     for i in "${!tag_exts_ubi[@]}"; do
-#         docker_dir="${IMAGE_ROOT}/kernel"
-#         full_path="${docker_dir}/Dockerfile.${file_exts_ubi[$i]}"
-#         if [[ -f "${full_path}" ]]; then
-#             build_tag="${REPO}:full-${tag_exts_ubi[$i]}"
-
-#             echo "****** Building image ${build_tag}..."
-#             $DOCKER build --no-cache=true "${build_tag}" -f "${full_path}"
-#         else
-#             echo "Could not find Dockerfile at path ${full_path}"
-#             exit 1
-#         fi
-#     done
-# fi
