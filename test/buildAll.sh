16cfd645dd0d0f1088a1e3af47d28bff5058e79f
Justeen-Randev <Justeen.Randev@ibm.com>
2019-12-10 15:45:37 -0500

    Building latest images

--- test/buildAll.sh
+++ test/buildAll.sh
@@ -12,7 +12,7 @@
 
 currentRelease=$1
 readonly REPO="websphere-liberty"
-readonly IMAGE_ROOT="ga/latest"
+readonly IMAGE_ROOT="../ga/latest"
 
 # Default to podman where available, docker otherwise.
 # Override by setting the DOCKER environment variable.
@@ -48,12 +48,12 @@ else
 
   for i in "${!tag_exts_ubi[@]}"; do
       docker_dir="${IMAGE_ROOT}/kernel"
-      full_path="${docker_dir}"
+      full_path="${docker_dir}/Dockerfile.${file_exts_ubi[$i]}"
       if [[ -f "${full_path}" ]]; then
           build_tag="${REPO}:full-${tag_exts_ubi[$i]}"
 
           echo "****** Building image ${build_tag}..."
-          $DOCKER build --no-cache=true "${build_tag}" -f "${full_path}/Dockerfile.${file_exts_ubi[$i]}"
+          $DOCKER build --no-cache=true "${build_tag}" -f "${full_path}"
       else
           echo "Could not find Dockerfile at path ${full_path}"
           exit 1
