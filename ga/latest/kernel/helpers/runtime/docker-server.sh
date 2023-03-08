b3b0ed3f7be88851e8fea021c66b17508fc92cbb
Thomas Watson <tjwatson@us.ibm.com>
2023-03-08 07:08:12 -0600

    Add InstantOn to GA images
    As of now the Ubuntu semeru images do not have CRIU
    support.  Regardless we keep the UBI and Ubuntu images
    consistent with our own content.  Once Semeru support is
    available for Ubuntu then we will have Liberty pieces in
    place to support InstantOn.
    
    For UBI based images the Semeru images only have CRIU
    support for openjdk11 and openjdk17 varients.  Support
    for openjdk8 is expected later. Similar to Ubuntu images
    we are keeping the Liberty content consistent across the
    different images with respect to InstantOn.  Once the
    Semeru images we are based on get the CRIU support then
    we will have the pieces in place across the Liberty image
    varients.

--- ga/latest/kernel/helpers/runtime/docker-server.sh
+++ ga/latest/kernel/helpers/runtime/docker-server.sh
@@ -142,4 +142,18 @@ fi
 
 
 # Pass on to the real server run
-exec "$@"
+if [ -d "/output/workarea/checkpoint/image" ]; then
+  # A checkpoint image found; exec dumb-init for signal handling.
+  # Use of dumb-init for PID 1 is required for signal handling because
+  # the restored server process cannot be PID 1.
+  exec dumb-init --rewrite 15:2 -- /opt/ibm/helpers/runtime/restore-server.sh "$@"
+elif [[ ! -z "$WLP_CHECKPOINT" ]]; then
+  # Unset WLP_CHECKPOINT so it is not set in the final image after checkpoint.
+  TMP_CHECKPOINT=$WLP_CHECKPOINT
+  unset WLP_CHECKPOINT
+  # A checkpoint action has been requested; run the checkpoint.sh script.
+  checkpoint.sh "$TMP_CHECKPOINT"
+else
+  # The default is to just exec the supplied CMD
+  exec "$@"
+fi
