080152034a3e1c65ab493ea00a94adf0e73f3b18
kabicin <kirbychin99@gmail.com>
2023-05-09 10:28:40 -0400

    Use short URL for building an app image doc

--- ga/23.0.0.4/kernel/helpers/build/features-installed.sh
+++ ga/23.0.0.4/kernel/helpers/build/features-installed.sh
@@ -5,5 +5,5 @@ if [ -f "/logs/configure-liberty.log" ]; then
   exit 0
 fi  
   
->&2 echo "WARNING: This is not an optimal build configuration. Although features in server.xml will continue to be installed correctly, the 'RUN features.sh' command should be added to the Dockerfile prior to configure.sh. See https://github.com/WASdev/ci.docker#building-an-application-image for a sample application image template."
+>&2 echo "WARNING: This is not an optimal build configuration. Although features in server.xml will continue to be installed correctly, the 'RUN features.sh' command should be added to the Dockerfile prior to configure.sh. See https://ibm.biz/wl-app-image-template for a sample application image template."
 exit 1 
