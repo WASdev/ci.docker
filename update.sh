11267ba3f2d16f44ae219175311db089a8fbb67a
Wendy Raschke <wraschke@us.ibm.com>
2023-10-25 15:33:07 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ echo "Hello from the update.sh script!"
 
 NEW_VERSION=23.0.0.66
 OLD_VERSION=23.0.0.11
+OLD_SHORT_VERSION=11
+BUILD_LABEL=12345_new
 
 echo "Copying latest files to $NEW_VERSION"
 cp -r ./ga/latest ./ga/$NEW_VERSION
@@ -16,10 +18,24 @@ for file in $(find ./ga/$NEW_VERSION -type f | xargs egrep -l "$searchString"); 
    # Perform the swap for each version string/label/SHA in order
    echo "--Performing subsitutions";
    sed -i'.bak' -e "s/$OLD_VERSION/$NEW_VERSION/" $file;
+   sed -i'.bak' -e "s/LIBERTY_BUILD_LABEL=*/LIBERTY_BUILD_LABEL=$BUILD_LABEL" $file;
+   sed -i'.bak' -e "s#ARG PARENT_IMAGE=icr.io/appcafe/websphere-liberty:kernel#ARG PARENT_IMAGE=icr.io/appcafe/websphere-liberty:$NEW_VERSION-kernel#g" $file
+   sed -i'.bak' -e "s#FROM websphere-liberty:kernel#FROM websphere-liberty:$VERSION-kernel#g" $file
 
     # Clean up temp files
     echo "--Removing temp files"
     rm $file.bak
 
-    echo;
+    cp ga/$OLD_VERSION/images.txt ga/$NEW_VERSION/images.txt;
+    sed -i'.bak' -e "s/$OLD_VERSION/$NEW_VERSION/g" ga/$NEW_VERSION/images.txt;
+    rm ga/$NEW_VERSION/images.txt.bak;
+
+    if [[ $(( $OLD_SHORT_VERSION % 3 )) -eq 0 ]]
+    then
+        :
+    else
+        rm -rf ga/$OLD_VERSION
+    fi
+
+    echo "Done processing new file updates!";
 done
