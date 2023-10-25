d7b6aed460ab0f9748076ef725264ec66c8af14b
Wendy Raschke <wraschke@us.ibm.com>
2023-10-25 16:33:59 -0500

    Add timestamp debug, cleanup

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Hello from the update.sh script!"
-echo "The latest version is really being invoked. :\ "
+echo $(date)
 
 NEW_VERSION=23.0.0.66
 OLD_VERSION=23.0.0.11
@@ -12,8 +12,6 @@ echo "Copying latest files to $NEW_VERSION"
 cp -r ./ga/latest ./ga/$NEW_VERSION
 
 # Perform the actual swaps by doing a search of the directory the script is running on
-searchString="$OLD_VERSION"
-#for file in $(find ./ga/$NEW_VERSION -type f | xargs egrep -l "$searchString"); do
 for file in $(find ./ga/$NEW_VERSION -name Dockerfile.*); do
    echo "Processing $file";
 
