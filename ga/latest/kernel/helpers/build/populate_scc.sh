8907ba85a609bdfe5c56dcb0a6501be41c8d96dc
Kirby Chin <37311900+kabicin@users.noreply.github.com>
2024-04-09 12:52:31 -0400

    Output errors to stderr by default when VERBOSE=true (#605)

--- ga/latest/kernel/helpers/build/populate_scc.sh
+++ ga/latest/kernel/helpers/build/populate_scc.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 if [ "$VERBOSE" != "true" ]; then
-  exec &>/dev/null
+  exec >/dev/null
 fi
 
 set -Eeox pipefail
