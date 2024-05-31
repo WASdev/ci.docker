44e26c4cc789ee0cde4587aa4c5319f509b5f78d
Kirby Chin <37311900+kabicin@users.noreply.github.com>
2024-05-31 12:47:36 -0700

    Use logger.sh

--- ga/latest/kernel/helpers/build/populate_scc.sh
+++ ga/latest/kernel/helpers/build/populate_scc.sh
@@ -12,9 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-if [ "$VERBOSE" != "true" ]; then
-  exec >/dev/null
-fi
+. /opt/ibm/helpers/build/internal/logger.sh
 
 set -Eeox pipefail
 
