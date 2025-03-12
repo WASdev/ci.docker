5829f0b7129c9950aead682eef1d5cbac3baf723
ilewis <idlewis@users.noreply.github.com>
2025-03-12 10:01:34 +0000

    Allow infinispan setup script to run on ubi minimal

--- ga/25.0.0.3/kernel/helpers/build/infinispan-client-setup.sh
+++ ga/25.0.0.3/kernel/helpers/build/infinispan-client-setup.sh
@@ -16,13 +16,20 @@
 
 set -Eeox pipefail
 
-yum update -y
-yum install -y maven
+pkgcmd=yum
+if ! command $pkgcmd
+then
+  pkgcmd=microdnf
+fi
+
+$pkgcmd update -y
+$pkgcmd install -y maven
 mkdir -p /opt/ibm/wlp/usr/shared/resources/infinispan
 echo '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">  <modelVersion>4.0.0</modelVersion>   <groupId>io.openliberty</groupId>  <artifactId>openliberty-infinispan-client</artifactId>  <version>1.0</version>  <!-- https://mvnrepository.com/artifact/org.infinispan/infinispan-jcache-remote -->  <dependencies>    <dependency>      <groupId>org.infinispan</groupId>      <artifactId>infinispan-jcache-remote</artifactId>      <version>10.1.3.Final</version>    </dependency>  </dependencies></project>' > /opt/ibm/wlp/usr/shared/resources/infinispan/pom.xml
 mvn -f /opt/ibm/wlp/usr/shared/resources/infinispan/pom.xml versions:use-latest-releases -DallowMajorUpdates=false
 mvn -f /opt/ibm/wlp/usr/shared/resources/infinispan/pom.xml dependency:copy-dependencies -DoutputDirectory=/opt/ibm/wlp/usr/shared/resources/infinispan
-yum remove -y maven
+# This fails with dependency errors using microdnf on ubi-minimal, but it is okay to let it fail
+yum remove -y maven || true
 rm -f /opt/ibm/wlp/usr/shared/resources/infinispan/pom.xml
 rm -f /opt/ibm/wlp/usr/shared/resources/infinispan/jboss-transaction-api*.jar
 rm -f /opt/ibm/wlp/usr/shared/resources/infinispan/reactive-streams-*.jar
