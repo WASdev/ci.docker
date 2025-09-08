7cfd249b95a07d4c0a4e53a4056f4c5182f89bda
kabicin <37311900+kabicin@users.noreply.github.com>
2025-09-08 12:58:16 -0400

    Add serviceability link to 25.0.0.9

--- ga/25.0.0.9/kernel/helpers/runtime/docker-server.sh
+++ ga/25.0.0.9/kernel/helpers/runtime/docker-server.sh
@@ -143,6 +143,13 @@ if [[ -n "$INFINISPAN_SERVICE_NAME" ]]; then
  echo "INFINISPAN_PASS: ${INFINISPAN_PASS}"
 fi
 
+# If SERVICEABILITY_NAMESPACE is set, link /liberty/logs to the serviceability directory
+if [[ ! -z "$SERVICEABILITY_NAMESPACE" ]] && [[ ! -z $HOSTNAME ]]; then
+  SERVICEABILITY_FOLDER="/serviceability/$SERVICEABILITY_NAMESPACE/$HOSTNAME/logs"
+  mkdir -p $SERVICEABILITY_FOLDER
+  rm /liberty/logs
+  ln -s $SERVICEABILITY_FOLDER /liberty/logs
+fi
 
 # Pass on to the real server run
 if [ -d "/output/workarea/checkpoint/image" ]; then
