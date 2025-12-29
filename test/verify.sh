1d9668d8fd3206f718f4c26acb6cf08ec4d320fd
Kirby Chin <37311900+kabicin@users.noreply.github.com>
2025-12-29 16:35:20 -0500

    Add ubi10 files

--- test/verify.sh
+++ test/verify.sh
@@ -23,23 +23,11 @@
 
 set -Eeo pipefail
 
-readonly USAGE="Usage: ./test.sh --image=<local-image> --repository=<docker-registry-repo>"
+readonly USAGE="Usage: ./verify.sh <local-image>"
+
+IMAGE=$1
 
 main () {
-   parse_args $@
-
-
-   case "$(uname -p)" in
-       "ppc64le")
-           readonly arch="ppc64le"
-           ;;
-       "s390x")
-           readonly arch="s390x"
-           ;;
-       *)
-           readonly arch="amd64"
-           ;;
-   esac
 
    local tests=$(declare -F | cut -d" " -f3 | grep "^test")
    echo "****** Testing ${IMAGE}..."
@@ -52,15 +40,15 @@ main () {
    done
 }
 
-## tests from all liberty docker repos
+## tests from all liberty podman repos
 waitForServerStart()
 {
    local cid=$1
    local count=${2:-1}
    local end=$((SECONDS+120))
-   while (( $SECONDS < $end && $(docker inspect -f {{.State.Running}} "${cid}") == "true" ))
+   while (( $SECONDS < $end && $(podman inspect -f {{.State.Running}} "${cid}") == "true" ))
    do
-      local result=$(docker logs "${cid}" 2>&1 | grep "CWWKF0011I" | wc -l)
+      local result=$(podman logs "${cid}" 2>&1 | grep "CWWKF0011I" | wc -l)
       if [ "${result}" = "${count}" ]
       then
          return 0
@@ -77,7 +65,7 @@ waitForServerStop()
    local end=$((SECONDS+120))
    while (( $SECONDS < $end ))
    do
-      local result=$(docker logs "${cid}" 2>&1 | grep "CWWKE0036I" | wc -l)
+      local result=$(podman logs "${cid}" 2>&1 | grep "CWWKE0036I" | wc -l)
       if [ $result = 1 ]
       then
          return 0
@@ -88,7 +76,7 @@ waitForServerStop()
    return 1
 }
 
-testDockerOnOpenShift()
+testpodmanOnOpenShift()
 {
    testLibertyStopsAndRestarts "OpenShift"
 }
@@ -99,9 +87,9 @@ testLibertyStopsAndRestarts()
     if [ "$1" == "OpenShift" ]; then
         timestamp=$(date '+%Y/%m/%d %H:%M:%S')
         echo "$timestamp *** testLibertyStopsAndRestarts on OpenShift"
-        local cid=$(docker run -d -u 1001:0 $IMAGE)
+        local cid=$(podman run -d -u 1001:0 $IMAGE)
     else
-        local cid=$(docker run -d $IMAGE)
+        local cid=$(podman run -d $IMAGE)
     fi
     echo "Waiting for server to start..."
     waitForServerStart "${cid}" \
@@ -110,13 +98,13 @@ testLibertyStopsAndRestarts()
     sleep 60
 
     echo "Stopping server..."
-    docker stop "${cid}" >/dev/null \
+    podman stop "${cid}" >/dev/null \
         || handle_test_failure "${cid}" "stopping"
     ## give server time to stop
     sleep 60
 
     echo "Starting the server again..."
-    docker restart "${cid}" >/dev/null \
+    podman restart "${cid}" >/dev/null \
         || handle_test_failure "${cid}" "starting"
 
     echo "Waiting for server to restart..."
@@ -127,51 +115,28 @@ testLibertyStopsAndRestarts()
     ## if it finds NO errors the grep "fails" as it found nothing
     ## therefore we make the conditional true in the catch
     local pass
-    docker logs "${cid}" 2>&1 | grep "ERROR" \
+    podman logs "${cid}" 2>&1 | grep "ERROR" \
         || pass="passed"; true
 
     if [[ -z "${pass}" ]]; then
         echo "Errors found in logs for container; exiting"
         echo "DEBUG START full log"
-        docker logs "${cid}"
+        podman logs "${cid}"
         echo "DEBUG END full log"
-        docker rm -f "${cid}" >/dev/null
+        podman rm -f "${cid}" >/dev/null
         exit 1
     fi
     echo "Removing container: ${cid}"
-    docker rm -f "${cid}" >/dev/null
+    podman rm -f "${cid}" >/dev/null
 }
 
 handle_test_failure () {
     local cid="$1"; shift
     local process="$1"
     echo "Error ${process} container or server; exiting"
-    docker logs "${cid}"
-    docker rm -f "${cid}" >/dev/null
+    podman logs "${cid}"
+    podman rm -f "${cid}" >/dev/null
     exit 1
 }
-parse_args() {
-    while [ $# -gt 0 ]; do
-        case "$1" in
-            --repository=*)
-                readonly REPO="${1#*=}"
-                ;;
-            --image=*)
-                ## currently unused variable but ideally dockerfiles take as arg
-                readonly IMAGE="${1#*=}"
-                ;;
-            *)
-                echo "Error: Invalid argument - $1"
-                echo "$USAGE"
-                exit 1
-        esac
-        shift
-    done
 
-    if [[ -z "${REPO}" || -z "${IMAGE}" ]]; then
-        echo "****** Error: missing required params"
-        echo "${USAGE}"
-        exit 1
-    fi
-}
 main $@
