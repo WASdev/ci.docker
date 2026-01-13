b10ebb66c9af2e798410cd7c36f3c18f396c867e
Wendy Raschke <wraschke@us.ibm.com>
2026-01-13 08:12:27 -0800

    Updates for 26.0.0.1

---
+++ ga/26.0.0.1/kernel/helpers/build/internal/logger.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# (C) Copyright IBM Corporation 2025.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function main() {
+    if [ "$VERBOSE" != "true" ]; then
+        exec >/dev/null
+    else
+        set -x
+    fi
+}
+
+function hideLogs() {
+    exec 3>&1 >/dev/null 4>&2 2>/dev/null
+}
+
+function showLogs() {
+    exec 1>&3 3>&- 2>&4 4>&-
+}
+
+main
