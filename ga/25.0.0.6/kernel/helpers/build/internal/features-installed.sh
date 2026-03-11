8e84f372bb4aea1844546630e94ec84766ff38c9
Gkerta Seferi <gkerta.sef@gmail.com>
2026-03-11 16:18:47 +0000

    Removing 25.0.0.6

--- ga/25.0.0.6/kernel/helpers/build/internal/features-installed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f "/logs/features.log" ]; then
-  rm /logs/features.log
-  exit 0
-fi  
-  
->&2 echo "WARNING: This is not an optimal build configuration. Although features in server.xml will continue to be installed correctly, the 'RUN features.sh' command should be added to the Dockerfile prior to configure.sh. See https://ibm.biz/wl-app-image-template for a sample application image template."
-exit 1 
